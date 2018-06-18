d15423cd5e9baf9d80836a12155bee484cb8bb5a
Ricardo N Feliciano <FelicianoTech@gmail.com>
2018-06-18 19:27:02 -0400

    Add version command. (#13)

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/felicianotech/md-proofer/cmd"
+import (
+	"github.com/spf13/viper"
+
+	"github.com/felicianotech/md-proofer/cmd"
+)
+
+var (
+	buildTime string = ""
+	gitCommit string = ""
+	version   string = "dev-build"
+)
 
 func main() {
+
+	viper.SetDefault("buildTime", buildTime)
+	viper.SetDefault("gitCommit", gitCommit)
+	viper.SetDefault("version", version)
+
 	cmd.Execute()
 }
